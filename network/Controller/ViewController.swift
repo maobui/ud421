f0a10b3a69b9ce5eddf37de4e66018014024d1d1
m.bui <mao.buidinh@hotmail.com>
2021-07-09 00:00:10 +0700

    feat: load image.

---
+++ network/Controller/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  network
+//
+//  Created by m.bui on 7/8/21.
+//
+
+import UIKit
+
+enum KittenImageLocation: String {
+    case http = "http://www.kittenswhiskers.com/wp-content/uploads/sites/23/2014/02/Kitten-playing-with-yarn.jpg"
+    case https = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Kitten_in_Rizal_Park%2C_Manila.jpg/460px-Kitten_in_Rizal_Park%2C_Manila.jpg"
+    case error = "not a url"
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    let imageLocation =
+        KittenImageLocation.http.rawValue
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func handleLoadImageButtonPress(_ sender: Any) {
+        guard let imageUrl = URL(string: imageLocation) else {
+            print("Cannot create URL")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+            guard let data = data else {
+                print("no data, or there was an error")
+                return
+            }
+            let downloadImage = UIImage(data: data)
+            self.imageView.image = downloadImage
+        }
+        task.resume()
+    }
+
+}
+
