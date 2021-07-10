79ec363b75b5f37d2923fc5885959524764b142f
m.bui <mao.buidinh@hotmail.com>
2021-07-10 16:23:36 +0700

    feat: parse json cont.

--- network/Controller/ViewController.swift
+++ network/Controller/ViewController.swift
@@ -20,15 +20,10 @@ class ViewController: UIViewController {
                 print("No data")
                 return
             }
-            print(data)
 
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                let url = json["message"] as! String
-                print(url)
-            } catch {
-                print(error)
-            }
+            let decoder = JSONDecoder()
+            let imgageData = try! decoder.decode(DogImage.self, from: data)
+            print(imgageData)
 
         }
         task.resume()
---
+++ network/Model/DogImage.swift
@@ -0,0 +1,14 @@
+//
+//  DogImage.swift
+//  network
+//
+//  Created by m.bui on 7/10/21.
+//
+
+import Foundation
+
+struct DogImage: Codable {
+    let status: String
+    let message: String
+}
+
