7749837a7c449e28ae933ff0e0d7a516f08b90bd
m.bui <mao.buidinh@hotmail.com>
2021-07-12 22:25:37 +0700

    feat: picker view.

--- network/Controller/ViewController.swift
+++ network/Controller/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickerView: UIPickerView!
+
+    let breeds: [String] = ["greyhound", "poodle"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        DogAPI.requestRandomImage(completionHandler: self.handleRandomImageResponse(imageData: error:))
+
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
 
     func handleRandomImageResponse(imageData: DogImage?, error: Error?) {
@@ -28,5 +33,23 @@ class ViewController: UIViewController {
         DispatchQueue.main.async {
             self.imageView.image = image
         }
+    }
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return breeds.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        breeds[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        DogAPI.requestRandomImage(completionHandler: self.handleRandomImageResponse(imageData: error:))
     }
 }
